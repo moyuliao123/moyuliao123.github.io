4f691f7a5914378de1ff3c052f66ba6106ea644f
Alyssa <moyuliao123@gmail.com>
2025-05-16 18:21:33 +0800

    Update script.js

--- MiYangPet/script.js
+++ MiYangPet/script.js
@@ -2,10 +2,9 @@ const container = document.getElementById('hover-zone');
 
 document.addEventListener('mousemove', (e) => {
   const rect = container.getBoundingClientRect();
-  const dist = Math.hypot(
-    e.clientX - (rect.left + rect.width / 2),
-    e.clientY - (rect.top + rect.height / 2)
-  );
+  const centerX = rect.left + rect.width / 2;
+  const centerY = rect.top + rect.height / 2;
+  const dist = Math.hypot(e.clientX - centerX, e.clientY - centerY);
 
   if (dist < 150) {
     container.classList.add('show');
